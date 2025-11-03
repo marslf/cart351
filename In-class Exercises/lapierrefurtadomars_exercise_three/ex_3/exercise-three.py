4bbf183d6628b4e36359a4c62a242ed278be4e65
marslf <mars.lf21@gmail.com>
2025-11-03 18:22:39 -0500

    comments

--- In-class Exercises/lapierrefurtadomars_exercise_three/ex_3/exercise-three.py
+++ In-class Exercises/lapierrefurtadomars_exercise_three/ex_3/exercise-three.py
@@ -17,13 +17,15 @@ def index():
 def t2():
     return render_template("t2.html")
 
+#route to receive star data and save to file
 @app.route("/postDataFetch", methods=['POST'])
 def postDataFetch():
     data = request.get_json()
-    stars = data.get("stars", [])
+    stars = data.get("stars", []) #get star coords
 
     filepath = os.path.join("files", "data.txt")
 
+    #convert data to JSON string with indentation
     data_to_save = {"stars": stars}
     data_s = json.dumps(data_to_save, indent=4)
 
@@ -31,7 +33,7 @@ def postDataFetch():
     with open(filepath, "a") as f:
         f.write(data_s + "\n")  # Add newline between constellations
 
-    print("Saved stars:", stars)
+    print("Saved stars:", stars) #log in terminal
 
     # Send JSON response to client
     return jsonify({"message": f"✨ Saved {len(stars)} stars as a constellation!"})
